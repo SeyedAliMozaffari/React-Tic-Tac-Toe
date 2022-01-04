11b1c94f14987d184d2d36c3ba90922144654c93
SeyedAliMozaffari <97095914+SeyedAliMozaffari@users.noreply.github.com>
2022-01-04 14:51:55 +0330

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+
+function App() {
+  return (
+    <div className='card'>
+		<div>
+			<button className='btn'>Delete</button>
+			<button className='btn'>Delete</button>
+			<button className='btn'>Delete</button>
+		</div>
+		<div>
+			<button className='btn' >Delete</button>
+			<button className='btn'>Delete</button>
+			<button className='btn'>Delete</button>
+		</div>
+		<div>
+			<button className='btn'>Delete</button>
+			<button className='btn'>Delete</button>
+			<button className='btn'>Delete</button>
+		</div>
+	</div>
+  );
+}
+
+export default App;
---
+++ src/test001.js
@@ -0,0 +1,7 @@
+function test(props){
+	return
+	<div>
+		
+	</div>
+}
+export default test;
